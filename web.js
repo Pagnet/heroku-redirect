83f8bca637e9d6556d32330c14c4c31575e2c395
Saulo Santiago <saulodasilvasantiago@gmail.com>
2018-07-12 19:59:37 -0300

    enable ssl

--- web.js
+++ web.js
@@ -1,9 +1,13 @@
+var sslRedirect = require('heroku-ssl-redirect');
 var express = require('express');
 var app = express();
 
 var newBaseURL = process.env.NEW_BASE_URL || 'http://example.com';
 var redirectStatus = parseInt(process.env.REDIRECT_STATUS || 302);
 var port = process.env.PORT || 5000;
+
+// enable ssl redirect
+app.use(sslRedirect());
 
 app.get('*', function(request, response) {
   response.redirect(redirectStatus, newBaseURL + request.url);
