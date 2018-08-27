0b304baa69fa52573e62f286d48f2b3288d2d79e
Saulo Santiago <saulodasilvasantiago@gmail.com>
2018-08-27 19:20:01 -0300

    add /.well-known/acme-challenge/ checker

--- web.js
+++ web.js
@@ -5,6 +5,9 @@ var newBaseURL = process.env.NEW_BASE_URL || 'http://example.com';
 var redirectStatus = parseInt(process.env.REDIRECT_STATUS || 302);
 var port = process.env.PORT || 5000;
 
+app.get('/.well-known/acme-challenge/*', function(request, response) {
+  response.send(process.env.certbot_secret)
+});
 
 app.get('*', function(request, response) {
   response.redirect(redirectStatus, newBaseURL + request.url);
